acb2399705129db9d555df7290d42ef635a66342
JoeCalvi <joecalvi@me.com>
2023-08-17 16:57:00 -0600

    fixed navbar collapse

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,6 @@
 <template>
     <nav>
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Dead By Daylight Loadout Builder</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
