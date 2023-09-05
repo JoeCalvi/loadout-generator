8bcb4d6b04bde220fde750901d8a9663751a5040
JoeCalvi <joecalvi@me.com>
2023-09-05 15:51:33 -0600

    survivor/killer in new tab if clicked

--- src/components/PerkBreakdown.vue
+++ src/components/PerkBreakdown.vue
@@ -54,9 +54,17 @@ export default {
               </div>
               <div class="col-md-9 mb-3 d-flex flex-column justify-content-center">
                 <h5 v-if="loadout[0]" class="mb-0">{{ loadout[0].name }}</h5>
-                <p v-if="loadout[0] && loadout[0].role == 'Survivor' && loadout[0].survivor"><span class="text-danger selectable" @click="getSurvivorById(`${loadout[0].survivor_id}`)">{{ loadout[0].survivor.name }}</span> Perk</p>
+                <p v-if="loadout[0] && loadout[0].role == 'Survivor' && loadout[0].survivor">
+                  <router-link class="text-danger text-decoration-none" :to="{ name: 'survivor', params: { survivorId: loadout[0].survivor_id }}" target="_blank">
+                    <span class="selectable" @click="getSurvivorById(`${loadout[0].survivor_id}`)">{{ loadout[0].survivor.name }}</span>
+                  </router-link>
+                   Perk</p>
                 <p v-if="loadout[0] && loadout[0].role == 'Survivor' && !loadout[0].survivor">General Survivor Perk</p>
-                <p v-if="loadout[0] && loadout[0].role == 'Killer' && loadout[0].killer"><span class="text-danger selectable" @click="getKillerById(`${loadout[0].killer_id}`)">{{ loadout[0].killer.killer_name }}</span> Perk</p>
+                <p v-if="loadout[0] && loadout[0].role == 'Killer' && loadout[0].killer">
+                  <router-link class="text-danger text-decoration-none" :to="{ name: 'killer', params: { killerId: loadout[0].killer_id }}" target="_blank">
+                    <span class="selectable" @click="getKillerById(`${loadout[0].killer_id}`)">{{ loadout[0].killer.killer_name }}</span>
+                  </router-link>
+                   Perk</p>
                 <p v-if="loadout[0] && loadout[0].role == 'Killer' && !loadout[0].killer">General Killer Perk</p>
                 <i v-if="loadout[0]">{{ loadout[0].description }}</i>
                 <div class="row mt-3" v-if="loadout[0] && loadout[0].associated_status_effects.length > 0">
@@ -82,9 +90,17 @@ export default {
               </div>
               <div class="col-md-9 mb-3 d-flex flex-column justify-content-center">
                 <h5 v-if="loadout[1]" class="mb-0">{{ loadout[1].name }}</h5>
-                <p v-if="loadout[1] && loadout[0].role == 'Survivor' && loadout[1].survivor"><span class="text-danger selectable" @click="getSurvivorById(`${loadout[1].survivor_id}`)">{{ loadout[1].survivor.name }}</span> Perk</p>
+                <p v-if="loadout[1] && loadout[0].role == 'Survivor' && loadout[1].survivor">
+                  <router-link class="text-danger text-decoration-none" :to="{ name: 'survivor', params: { survivorId: loadout[1].survivor_id }}" target="_blank">
+                    <span class="selectable" @click="getSurvivorById(`${loadout[1].survivor_id}`)">{{ loadout[1].survivor.name }}</span>
+                  </router-link>
+                   Perk</p>
                 <p v-if="loadout[1] && loadout[0].role == 'Survivor' && !loadout[1].survivor">General Survivor Perk</p>
-                <p v-if="loadout[1] && loadout[1].role == 'Killer' && loadout[1].killer"><span class="text-danger selectable" @click="getKillerById(`${loadout[1].killer_id}`)">{{ loadout[1].killer.killer_name }}</span> Perk</p>
+                <p v-if="loadout[1] && loadout[1].role == 'Killer' && loadout[1].killer">
+                  <router-link class="text-danger text-decoration-none" :to="{ name: 'killer', params: { killerId: loadout[1].killer_id }}" target="_blank">
+                    <span class="selectable" @click="getKillerById(`${loadout[1].killer_id}`)">{{ loadout[1].killer.killer_name }}</span>
+                  </router-link>
+                   Perk</p>
                 <p v-if="loadout[1] && loadout[1].role == 'Killer' && !loadout[1].killer">General Killer Perk</p>
                 <i v-if="loadout[1]">{{ loadout[1].description }}</i>
                 <div class="row mt-3" v-if="loadout[1] && loadout[1].associated_status_effects.length > 0">
@@ -110,9 +126,17 @@ export default {
               </div>
               <div class="col-md-9 mb-3 d-flex flex-column justify-content-center">
                 <h5 v-if="loadout[2]" class="mb-0">{{ loadout[2].name }}</h5>
-                <p v-if="loadout[2] && loadout[0].role == 'Survivor' && loadout[2].survivor"><span class="text-danger selectable" @click="getSurvivorById(`${loadout[2].survivor_id}`)">{{ loadout[2].survivor.name }}</span> Perk</p>
+                <p v-if="loadout[2] && loadout[0].role == 'Survivor' && loadout[2].survivor">
+                  <router-link class="text-danger text-decoration-none" :to="{ name: 'survivor', params: { survivorId: loadout[2].survivor_id }}" target="_blank">
+                    <span class="selectable" @click="getSurvivorById(`${loadout[2].survivor_id}`)">{{ loadout[2].survivor.name }}</span>
+                  </router-link>
+                   Perk</p>
                 <p v-if="loadout[2] && loadout[0].role == 'Survivor' && !loadout[2].survivor">General Survivor Perk</p>
-                <p v-if="loadout[2] && loadout[2].role == 'Killer' && loadout[2].killer"><span class="text-danger selectable" @click="getKillerById(`${loadout[2].killer_id}`)">{{ loadout[2].killer.killer_name }}</span> Perk</p>
+                <p v-if="loadout[2] && loadout[2].role == 'Killer' && loadout[2].killer">
+                  <router-link class="text-danger text-decoration-none" :to="{ name: 'killer', params: { killerId: loadout[2].killer_id }}" target="_blank">
+                    <span class="selectable" @click="getKillerById(`${loadout[2].killer_id}`)">{{ loadout[2].killer.killer_name }}</span>
+                  </router-link>
+                   Perk</p>
                 <p v-if="loadout[2] && loadout[2].role == 'Killer' && !loadout[2].killer">General Killer Perk</p>
                 <i v-if="loadout[2]">{{ loadout[2].description }}</i>
                 <div class="row mt-3" v-if="loadout[2] && loadout[2].associated_status_effects.length > 0">
@@ -138,9 +162,17 @@ export default {
               </div>
               <div class="col-md-9 mb-3 d-flex flex-column justify-content-center">
                 <h5 v-if="loadout[3]" class="mb-0">{{ loadout[3].name }}</h5>
-                <p v-if="loadout[3] && loadout[0].role == 'Survivor' && loadout[3].survivor"><span class="text-danger selectable" @click="getSurvivorById(`${loadout[3].survivor_id}`)">{{ loadout[3].survivor.name }}</span> Perk</p>
+                <p v-if="loadout[3] && loadout[0].role == 'Survivor' && loadout[3].survivor">
+                  <router-link class="text-danger text-decoration-none" :to="{ name: 'survivor', params: { survivorId: loadout[3].survivor_id }}" target="_blank">
+                    <span class="text-danger selectable" @click="getSurvivorById(`${loadout[3].survivor_id}`)">{{ loadout[3].survivor.name }}</span>
+                  </router-link>
+                   Perk</p>
                 <p v-if="loadout[3] && loadout[0].role == 'Survivor' && !loadout[3].survivor">General Survivor Perk</p>
-                <p v-if="loadout[3] && loadout[3].role == 'Killer' && loadout[3].killer"><span class="text-danger selectable" @click="getKillerById(`${loadout[3].killer_id}`)">{{ loadout[3].killer.killer_name }}</span> Perk</p>
+                <p v-if="loadout[3] && loadout[3].role == 'Killer' && loadout[3].killer">
+                  <router-link class="text-danger text-decoration-none" :to="{ name: 'killer', params: { killerId: loadout[3].killer_id }}" target="_blank">
+                    <span class="selectable" @click="getKillerById(`${loadout[3].killer_id}`)">{{ loadout[3].killer.killer_name }}</span>
+                  </router-link>
+                   Perk</p>
                 <p v-if="loadout[3] && loadout[3].role == 'Killer' && !loadout[3].killer">General Killer Perk</p>
                 <i v-if="loadout[3]">{{ loadout[3].description }}</i>
                 <div class="row mt-3" v-if="loadout[3] && loadout[3].associated_status_effects.length > 0">
@@ -165,18 +197,30 @@ export default {
         <h1 v-if="activeSurvivor != null && activeKiller == null" class="modal-title fs-5" id="activePerkModalLabel">{{ activeSurvivor.name }}</h1>
       </div>
       <div class="modal-body">
-        <div class="container p-3">
-          <div class="row m-auto">
-            <div class="col-2 d-flex align-items-center justify-content-center">
-              <div class="perk-icon-background d-flex justify-content-center align-items-center m-3">
+        <div class="container-fluid p-3">
+          <div class="row w-100 m-auto">
+            <div class="col-md-4 d-flex align-items-center justify-content-center my-2">
                 <img v-if="activeSurvivor != null && activeKiller == null" :src="activeSurvivor.portrait" :alt="activeSurvivor.name" :title="activeSurvivor.name">
+            </div>
+            <div class="col-md-8 d-flex flex-column align-items-center">
+              <div class="row w-100 m-auto">
+                <div class="col-md-4 d-flex justify-content-center p-3 mb-3">
+                  <div class="perk-icon-background d-flex justify-content-center align-items-center">
+                    <img v-if="activeSurvivor.perk_one" class="perk-icon" :src="activeSurvivor.perk_one.icon" :alt="activeSurvivor.perk_one.name" :title="activeSurvivor.perk_one.name">
+                  </div>
+                </div>
+                <div class="col-md-4 d-flex justify-content-center p-3 mb-3">
+                  <div class="perk-icon-background d-flex justify-content-center align-items-center">
+                    <img v-if="activeSurvivor.perk_two" class="perk-icon" :src="activeSurvivor.perk_two.icon" :alt="activeSurvivor.perk_two.name" :title="activeSurvivor.perk_two.name">
+                  </div>
+                </div>
+                <div class="col-md-4 d-flex justify-content-center p-3 mb-3">
+                  <div class="perk-icon-background d-flex justify-content-center align-items-center">
+                    <img v-if="activeSurvivor.perk_three" class="perk-icon" :src="activeSurvivor.perk_three.icon" :alt="activeSurvivor.perk_three.name" :title="activeSurvivor.perk_three.name">
+                </div>
               </div>
             </div>
-            <div class="col-10 d-flex justify-content-center align-items-center">
-              <div class="row m-auto">
-                
-              </div>
-            </div>
+          </div>
           </div>
         </div>
       </div>
