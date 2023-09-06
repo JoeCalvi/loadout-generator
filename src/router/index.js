c4b6db88eb1b3efc150c1baa072a91e51bf69b8e
JoeCalvi <joecalvi@me.com>
2023-09-05 20:23:46 -0600

    hash

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import KillerView from '../views/KillerView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
+  mode: 'hash',
   routes: [
     {
       path: '/',
